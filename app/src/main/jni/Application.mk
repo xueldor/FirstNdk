3a2bcdef1d165454cf89ecf8f6f2e13c646ecdc4
xuexiangyu <xuexiangyu@hangsheng.com.cn>
2019-08-26 18:32:25 +0800

    补充针对eclipse的说明

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,3 +1,7 @@
 APP_MODULES := JniLib
 APP_ABI := all
-APP_STL := c++_static
+APP_STL := c++_static #使用了C++ STL
+
+#过去在eclipse上用较老版本的NDK调试时，使用以下配置。现在已经不需要
+#APP_STL := stlport_static
+#APP_CFLAGS += -Wno-error=format-security
