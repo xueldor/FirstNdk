254bd20911f2e8a8e0c0f6075de4a1c72965cb81
xuexiangyu <xuexiangyu@hangsheng.com.cn>
2019-09-04 18:43:42 +0800

    修复Android.mk中的一处错误

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := JniLib
-LOCAL_SRC_FILES =: Jni2Lib.cpp JniLib.cpp Jni3Lib.cpp
+LOCAL_SRC_FILES := Jni2Lib.cpp JniLib.cpp Jni3Lib.cpp
 
 #使用__android_log_print需要添加
 LOCAL_LDLIBS+= -L$(SYSROOT)/usr/lib -llog
